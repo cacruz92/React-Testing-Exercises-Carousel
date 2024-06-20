e7d96da278d320b0a2c5cfe430b8d5c351902439
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-20 15:19:46 -0400

    added remaining files

---
+++ src/Card.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card";
+
+// smoke test
+
+it("renders without crashing", function () {
+    render(<Card />)
+})
